300ae5128794212409e787f3c0ca2e6c8fa4cf79
sergzaligin359 <webdevjsphp@gmail.com>
2020-07-06 13:48:56 +0300

    fix

--- carusel/main.js
+++ carusel/main.js
@@ -2,4 +2,11 @@ console.log('dataImages', dataImages);
 
 const carusel = document.querySelector('.carusel');
 
+const dataCarusel = {
+    data: dataImages,
+    currentData: [...data].splice(0, 4),
+    pageItems: 4
+};
+
 console.log('carusel', carusel);
+console.log('dataCarusel', dataCarusel);
