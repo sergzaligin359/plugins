7524d7ff13120fbff30d6ca6e0fdca034a16447f
sergzaligin359 <webdevjsphp@gmail.com>
2020-07-06 14:21:44 +0300

    fix

--- carusel/main.js
+++ carusel/main.js
@@ -1,12 +1,103 @@
 console.log('dataImages', dataImages);
 
+const caruselWrapper = document.querySelector('.carusel-wrapper');
 const carusel = document.querySelector('.carusel');
 
 const dataCarusel = {
     data: dataImages,
-    currentData: [...data].splice(0, 4),
+    currentData: [...dataImages].splice(0, 4),
     pageItems: 4
 };
 
+const pagination = (data) => {
+    const items = data.data.length;
+    const offset = data.pageItems;
+    const pages = Math.ceil(items / offset);
+    let start = 0;
+
+    const back = document.querySelector('.back');
+    const forward = document.querySelector('.forward');
+    
+    forward.dataset.start = 0;
+    back.dataset.start = 0;
+    forward.dataset.page = 1;
+    back.dataset.page = 1;
+
+    back.addEventListener('click', () => {
+    
+        back.dataset.start = parseInt(forward.dataset.start) - offset;
+        forward.dataset.start = parseInt(back.dataset.start);
+        
+        if(parseInt(back.dataset.page) <= 1 || parseInt(back.dataset.start) <= 0){
+            console.warn('PAGE EQ!!!');
+            forward.dataset.start = 0;
+            forward.dataset.page = 1;
+            back.dataset.start = 0;
+            back.dataset.page = 1;
+            //back.setAttribute('disabled', true);
+        }
+
+
+        start = parseInt(back.dataset.start);
+
+        dataCarusel.currentData = [...data.data].splice(start, offset);
+        const carusel = document.querySelector('.carusel');
+        carusel.replaceWith(drawCarusel());
+    });
+    
+    
+    forward.addEventListener('click', () => {
+
+        forward.dataset.start = parseInt(forward.dataset.start) + offset;
+        forward.dataset.page = parseInt(forward.dataset.page) + 1;
+        back.dataset.page = parseInt(forward.dataset.page);
+
+        if(parseInt(forward.dataset.page) > pages){
+            console.warn('PAGE EQ!!!');
+            forward.dataset.start = 0;
+            forward.dataset.page = 1;
+        }
+
+        start =  parseInt(forward.dataset.start);
+
+
+        dataCarusel.currentData = [...data.data].splice(start, offset);
+        console.log('dataCarusel.currentData', dataCarusel.currentData);
+        const carusel = document.querySelector('.carusel');
+        carusel.replaceWith(drawCarusel());
+    });
+
+    
+};
+
+const drawCarusel = () => {
+
+    const div = document.createElement('div');
+    div.className = 'carusel';
+    
+    dataCarusel.currentData.forEach(el => {
+
+        const figure = document.createElement('figure');
+
+        const img = document.createElement('img');
+        img.src = el.src;
+        const figcaption = document.createElement('figcaption');
+        figcaption.innerHTML = `${el.title}:  <a href="#">подробнее</a>`;
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+
+        div.appendChild(figure);
+
+    });
+
+    return div;
+};
+
+//carusel.appendChild(drawCarusel());
+carusel.replaceWith(drawCarusel());
+
 console.log('carusel', carusel);
 console.log('dataCarusel', dataCarusel);
+
+pagination(dataCarusel);
